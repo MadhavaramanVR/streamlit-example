b0c768e923549efe041e14d47d491cc908de4746
Madhavaraman V <101283753+MadhavaramanVR@users.noreply.github.com>
2023-02-27 18:30:43 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,107 +1,68 @@
-import streamlit as st
-import pandas as pd
-
-st.set_page_config(page_icon="📈", page_title="Crypto Dashboard")
-
-st.sidebar.image(
-    "https://res.cloudinary.com/crunchbase-production/image/upload/c_lpad,f_auto,q_auto:eco,dpr_1/z3ahdkytzwi1jxlpazje",
-    width=50,
-)
-
-c1, c2 = st.columns([1, 8])
-
-with c1:
-    st.image(
-        "https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/240/apple/285/chart-increasing_1f4c8.png",
-        width=90,
-    )
-
-st.markdown(
-    """# **Crypto Dashboard**
-A simple cryptocurrency price app pulling price data from the [Binance API](https://www.binance.com/en/support/faq/360002502072), from [Data Professor](https://twitter.com/thedataprof).
 """
-)
+Cryptocurency Daily Prices 
+Dashboard using streamlit and Yahoo Finance libraries
+@Bek Brace 
+Almost the same thing we did with stock prices in previous tutorial
+"""
 
-st.header("**Selected Price**")
-
-# Load market data from Binance API
-df = pd.read_json("https://api.binance.com/api/v3/ticker/24hr")
-
-# Custom function for rounding values
-def round_value(input_value):
-    if input_value.values > 1:
-        a = float(round(input_value, 2))
-    else:
-        a = float(round(input_value, 8))
-    return a
+import yfinance as yf  # Yahoo finance to get stock data
+import streamlit as st  # Streamlit to create the webapp
+from PIL import Image  # Import Pillow to add icons - Python Image Library
+from urllib.request import urlopen  # To add URLS
 
 
-crpytoList = {
-    "Price 1": "BTCBUSD",
-    "Price 2": "ETHBUSD",
-    "Price 3": "BNBBUSD",
-    "Price 4": "XRPBUSD",
-    "Price 5": "ADABUSD",
-    "Price 6": "DOGEBUSD",
-    "Price 7": "SHIBBUSD",
-    "Price 8": "DOTBUSD",
-    "Price 9": "MATICBUSD",
-}
-
-col1, col2, col3 = st.columns(3)
-
-for i in range(len(crpytoList.keys())):
-    selected_crypto_label = list(crpytoList.keys())[i]
-    selected_crypto_index = list(df.symbol).index(crpytoList[selected_crypto_label])
-    selected_crypto = st.sidebar.selectbox(
-        selected_crypto_label, df.symbol, selected_crypto_index, key=str(i)
-    )
-    col_df = df[df.symbol == selected_crypto]
-    col_price = round_value(col_df.weightedAvgPrice)
-    col_percent = f"{float(col_df.priceChangePercent)}%"
-    if i < 3:
-        with col1:
-            st.metric(selected_crypto, col_price, col_percent)
-    if 2 < i < 6:
-        with col2:
-            st.metric(selected_crypto, col_price, col_percent)
-    if i > 5:
-        with col3:
-            st.metric(selected_crypto, col_price, col_percent)
-
-st.header("")
-
-# st.download_button(
-#    label="Download data as CSV",
-#    data=df,
-#    #file_name='large_df.csv',
-#    # mime='text/csv'
-#    )
+# Define bitcoin and ethereum and other cryptocurrency abbreviation used by Yahoo Finance.
+Bitcoin = 'BTC-USD'
+Ethereum = 'ETH-USD'
+Ripple = 'XRP-USD'
+Bitcoin_Cash = 'BCH-USD'
 
 
-@st.cache
-def convert_df(df):
-    # IMPORTANT: Cache the conversion to prevent computation on every rerun
-    return df.to_csv().encode("utf-8")
+# Lets access the Data from Yahoo Finance using .ticker method
+BTC_Data = yf.Ticker(Bitcoin)
+ETH_Data = yf.Ticker(Ethereum)
+XRP_Data = yf.Ticker(Ripple)
+BCH_Data = yf.Ticker(Bitcoin_Cash)
 
 
-csv = convert_df(df)
+# Lets fetch the Price History from Yahoo Finance. We use Max Period to get all the History
+BTCHis = BTC_Data.history(period="max")
+ETHHis = ETH_Data.history(period="max")
+XRPHis = XRP_Data.history(period="max")
+BCHHis = BCH_Data.history(period="max")
 
-st.download_button(
-    label="Download data as CSV",
-    data=csv,
-    file_name="large_df.csv",
-    mime="text/csv",
-)
+st.title(" Cryptocurrency Daily Prices ₿ ")
+st.header("Your Dashboard ✨")
+st.subheader("You can add more crypto in the code </>")
 
-st.dataframe(df, height=2000)
+#Bitcoin
+st.write(""" ## Bitcoin ($) """)
+# Adding icon for BTC
+imageBTC = Image.open(urlopen('https://s2.coinmarketcap.com/static/img/coins/64x64/1.png'))
+st.image(imageBTC)
+# Create a line chart from BTC-USD Close Price history
+st.line_chart(BTCHis.Close,  use_container_width=True)
 
+#Ethereum
+st.write(""" ## Ethereum ($) """)
+# Adding icon for ETH
+imageETH = Image.open(urlopen('https://s2.coinmarketcap.com/static/img/coins/64x64/1027.png'))
+st.image(imageETH, use_column_width=False)
+# Create a line chart from ETH-USD Close Price history
+st.line_chart(ETHHis.Close,  use_container_width=True)
 
-st.markdown(
-    """
-<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
-""",
-    unsafe_allow_html=True,
-)
+#Ripple
+st.write(""" ## Ripple ($) """)
+# Adding icon for XRP
+imageXRP = Image.open(urlopen('https://s2.coinmarketcap.com/static/img/coins/64x64/52.png'))
+st.image(imageXRP, use_column_width=False)
+# Create a line chart from XRP-USD Close Price history
+st.line_chart(XRPHis.Close,  use_container_width=True)
+
+#Bitcoin Cash
+st.write(""" ## Bitcoin-Cash ($USD) """)
+# Adding icon for BCH
+imageBCH = Image.open(urlopen('https://s2.coinmarketcap.com/static/img/coins/64x64/1831.png'))
+st.image(imageBCH, use_column_width=False)
+# Create a line chart from BCH-USD Close Price history
+st.line_chart(BCHHis.Close,  use_container_width=True)
